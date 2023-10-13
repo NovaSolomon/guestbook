61f255b6771ae5b9709380a34a948ac9b35f916d
Nova <felix.melde@mailbox.tu-dresden.de>
2023-10-14 00:47:35 +0200

    Added support for an Email field

--- src/main/java/guestbook/Application.java
+++ src/main/java/guestbook/Application.java
@@ -60,12 +60,13 @@ public class Application {
 		return args -> {
 
 			Stream.of( //
-					new GuestbookEntry("H4xx0r", "first!!!"), //
-					new GuestbookEntry("Arni", "Hasta la vista, baby"), //
+					new GuestbookEntry("H4xx0r", "first!!!", "hacker@bruh.cap"), //
+					new GuestbookEntry("Arni", "Hasta la vista, baby", "Arni.nold@web.de"), //
 					new GuestbookEntry("Duke Nukem",
-							"It's time to kick ass and chew bubble gum. And I'm all out of gum."), //
+							"It's time to kick ass and chew bubble gum. And I'm all out of gum.", "dudebro@chadbot.com"), //
 					new GuestbookEntry("Gump1337",
-							"Mama always said life was like a box of chocolates. You never know what you're gonna get.")) //
+							"Mama always said life was like a box of chocolates. You never know what you're gonna get.",
+							"fast.boiii@murica.com")) //
 					.forEach(guestbook::save);
 		};
 	}
